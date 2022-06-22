9b57882601b671879138bcb817ef2efe2caa8ef1
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-06-22 18:05:49 +0200

    Modify printing inside utils.js

--- utils.js
+++ utils.js
@@ -1,9 +1,5 @@
 const moment = require("moment");
 exports.print_log = (file_name, message) => {
-    // console.log("[" + new Date().toLocaleString('it-IT') + "] FROM FILE "
-    //     + file_name + " LOG: " + message);
     console.log("[" + moment().format('YYYY/MM/DD - HH:mm:ss:SSS') + "] FROM FILE "
         + file_name + " LOG: " + message);
-    // console.log("[" + (new Date).getTime() + "] FROM FILE "
-    //     + file_name + " LOG: " + message);
 };
