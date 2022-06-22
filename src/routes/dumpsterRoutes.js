2554c3e25dcd5da87c2b2556861de9840195d5c7
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-06-22 18:07:33 +0200

    src/routes/dumpsterRoutes.js Add new route: close, activate, deactivate, isFull, lastWeight

--- src/routes/dumpsterRoutes.js
+++ src/routes/dumpsterRoutes.js
@@ -1,33 +1,34 @@
-/* La stringa è divers rispetto quella delle slide perchè il nome del controller fornito è diverso*/
 const dumpsterController = require('../controllers/dumpsterController');
-const utils = require('./../../utils');
 
-let this_file_name = "dumpsterRoutes.js";
+const utils = require('./../../utils');
+const this_file_name = "dumpsterRoutes.js";
 
 module.exports = (app) => {
     app.route('/')
         .get(dumpsterController.rootByGet)
-        .post(dumpsterController.extendTimer);
+
+    app.route('/data')
+        .get(dumpsterController.getData);
 
     app.route('/open')
         .get(dumpsterController.openDumpster)
         .post(dumpsterController.extendTimer);
 
-    app.route('/data')
-        .get(dumpsterController.getData);
-    //app.rout('/use')
-    // app.route('/movies')
-    //     .get(moviesController.readAllMovies)
-    //     .post(moviesController.createMovie);
-    //
-    // app.route('/movies/:id')
-    //      .get(dumpsterController.readMovie)
-    //     .put(moviesController.updateMovie)
-    //     .delete(moviesController.deleteMovie);
-    
-        //.get(moviesController.hello_world);
-        // .post(moviesController.rootByPost);
-    //app.routes('/movies')
-    //     .get(moviesController.readAllMovies)
-    //     .post(moviesController.createMovie)
+    app.route('/close')
+        .get(dumpsterController.closeDumpster);
+
+    app.route('/activate')
+        .get(dumpsterController.activate)
+
+    app.route('/deactivate')
+        .get(dumpsterController.deactivate)
+
+    app.route('/isFull')
+        .get(dumpsterController.isFull)
+
+    app.route('/lastWeight')
+        .get(dumpsterController.lastWeight)
+
+    // app.route('/simulateThrowing')
+    //     .get(dumpsterController.simulateThrowing)
 }
