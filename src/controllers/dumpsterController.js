7221bb98f1750bc2e944e2026f3704ac92f3619b
Angelo Tinti <angelo.tinti.30@gmail.com>
2022-06-22 17:26:31 +0200

    Add isFull() and lastWeight() in dumpsterController.js

--- src/controllers/dumpsterController.js
+++ src/controllers/dumpsterController.js
@@ -1,6 +1,6 @@
 const utils = require('./../../utils');
 //const moment = require('moment');
-const data = require('./../../data/dumpsterData.json');
+const dumpsterDataPath = './../../data/dumpsterData.json';
 const this_file_name = "dumpsterController.js";
 
 let myTimer;
@@ -12,7 +12,9 @@ function simulateDumpsterClosing(){
 function simulateDumpsterOpening(){
     utils.print_log(this_file_name, 'Dumpster opening...');
 }
-
+function getDumpsterData(){
+    return require(dumpsterDataPath);
+}
 function extendableTimeout(fn, time){
     const id = setTimeout(fn, time);
     const timeStart = new Date();
@@ -37,16 +39,18 @@ const rootByGet = (req,res) => {
 
 const getData = (req, res) => {
     utils.print_log(this_file_name,"getData requested");
-    res.send(data);
+    res.send(getDumpsterData());
 };
 
 const openDumpster = (req,res) => {
+    utils.print_log(this_file_name,"openDumpster requested");
     dumpsterIsBeingUsed = true;
     const openingTimeDumpster = 10000;
     simulateDumpsterOpening();
     myTimer = extendableTimeout(() => {
         utils.print_log(this_file_name,"Timer finished");
         simulateDumpsterClosing();
+        // TODO: set new last weight of dumpster
         dumpsterIsBeingUsed = false;
     }, openingTimeDumpster);
 
@@ -54,17 +58,21 @@ const openDumpster = (req,res) => {
 }
 
 const extendTimer = (req, res) => {
+    utils.print_log(this_file_name,"extendTimer requested");
     const timeData = req.body['time'];
     utils.print_log(this_file_name, "Timer to extend of: " + timeData + "ms");
     myTimer.extend(5000);
     res.send("Timer extended");
 }
 
-const closeDumpster = () => {
+const closeDumpster = (req,res) => {
+    utils.print_log(this_file_name,"closeDumpster requested");
     simulateDumpsterClosing();
+    res.send("Dumpster closing...");
 }
 
 const activate = (req,res) => {
+    utils.print_log(this_file_name,"activate dumpster requested");
     if(!dumpsterIsBeingUsed){
         // TODO: Effettuare la scrittura sul json dell'informazione
         res.json({dumpsterWasActivate: true});
@@ -74,6 +82,7 @@ const activate = (req,res) => {
 }
 
 const deactivate = (req,res) => {
+    utils.print_log(this_file_name,"deactivate dumpster requested");
     if(!dumpsterIsBeingUsed){
         // TODO: Effettuare la scrittura sul json dell'informazione
         res.json({dumpsterWasDeactivate: true});
@@ -82,7 +91,26 @@ const deactivate = (req,res) => {
     }
 }
 
+const isFull = (req,res) => {
+    utils.print_log(this_file_name,"isFull dumpster requested");
+    const data = getDumpsterData();
+    res.json({isFull:data.fillingPercentage >= data.usableUntilPercentage});
+}
 
+const lastWeight = (req,res) =>{
+    utils.print_log(this_file_name,"lastWeight  dumpster requested");
+    res.json({lastWeight:getDumpsterData().lastDropWasteWeight})
+}
+
+// const simulateThrowing = (req,res) => {
+//     utils.print_log(this_file_name,"req.query.weight sent " + req.query.weight)
+//     let wasteQuantityThrown = parseFloat(req.query.weight.replace(/,/,'.'))
+//     utils.print_log(this_file_name,"Number sent " + wasteQuantityThrown)
+//     utils.print_log(this_file_name,"Number sent " + (wasteQuantityThrown+1))
+//
+//
+//     res.send("All ok");
+// }
 
 module.exports = {
     rootByGet,
@@ -91,5 +119,8 @@ module.exports = {
     extendTimer,
     closeDumpster,
     activate,
-    deactivate
+    deactivate,
+    isFull,
+    lastWeight,
+    // simulateThrowing
 }
